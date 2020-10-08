eae4da98a2429c499576132f4d933aa4749b8ecb
Ronald Coarite <ronaldcoarite@gmail.com>
2020-10-07 22:02:07 -0400

    Agregando base de proyecto

---
+++ src/main/webapp/pages/home/home.controller.js
@@ -0,0 +1,13 @@
+/* global Thread */
+
+class HomePage extends Page {
+    // @Override
+    async onCreate(intent) {
+        this.setContentView("pages/home/home.view.xml");
+    }
+
+    // @Override
+    async onStart() {
+        
+    }
+};
