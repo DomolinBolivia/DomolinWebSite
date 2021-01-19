07fdd50199ebdb0f4d9225717f6d5bc167426587
Ronald Coarite <ronaldcoarite@gmail.com>
2021-01-19 15:32:11 +0000

    Mejoras en fondo de aplicacinon

--- src/main/webapp/assets/bg/bgLoader.js
+++ src/main/webapp/assets/bg/bgLoader.js
@@ -17,7 +17,8 @@ window.onload = function () {
         particles.vertices.push(particle);
     }
 
-    var particleTexture = THREE.ImageUtils.loadTexture('assets/bg/particle4B.png');
+    var particleTexture = new THREE.TextureLoader().load( 'assets/bg/particle4B.png' );
+    
     var pointsMaterial = new THREE.PointsMaterial({map: particleTexture, transparent: true, size: 5});
     var poinst = new THREE.Points(particles, pointsMaterial);
 
@@ -26,23 +27,42 @@ window.onload = function () {
 
     // RED
     const nroPoints = 50;
-    var pointsLines = new THREE.Geometry();
-    const geometry = new THREE.Geometry();
-    var material = new THREE.LineBasicMaterial({
+    const geometryPointLines = new THREE.Geometry();
+    const geometryLines = new THREE.Geometry();
+    var materialLines = new THREE.LineBasicMaterial({
         color: 0xb87d1d,
         transparent: true,
         opacity: 0.1,
         linewidth: 1
     });
 
+    let vector1 = null;
+    let mod=1;
     for (var p = 0; p < nroPoints; p++) {
-        let particle = new THREE.Vector3(Math.random() * 400 - 250, Math.random() * 400 - 250, Math.random() * 400 - 250);
-        geometry.vertices.push(particle);
-        pointsLines.vertices.push(particle.clone());
+        let vector3 = null;
+        switch (mod){
+            case 1:
+                vector3 = new THREE.Vector3(Math.random() * 400 - 250, Math.random() * 400 - 250, Math.random() * 400 - 250);
+                vector1 = vector3;
+                mod++;
+                geometryPointLines.vertices.push(vector3.clone());
+                break;
+            case 2:
+                vector3 = new THREE.Vector3(Math.random() * 400 - 250, Math.random() * 400 - 250, Math.random() * 400 - 250);
+                mod++;
+                geometryPointLines.vertices.push(vector3.clone());
+                break;
+            default:
+                vector3 = vector1;
+                mod=1;
+                p--;
+                break;
+        }
+        geometryLines.vertices.push(vector3);
     }
-    var texturePointLine = THREE.ImageUtils.loadTexture('assets/bg/light.png');
+    var texturePointLine = new THREE.TextureLoader().load('assets/bg/light.png');
     var pointsLineMaterial = new THREE.PointsMaterial({map: texturePointLine, transparent: true, size: 8});
-    var pointLine = new THREE.Points(pointsLines, pointsLineMaterial);
+    var pointLine = new THREE.Points(geometryPointLines, pointsLineMaterial);
     scene.add(pointLine);
 
 //                const light = new THREE.PointLight( 0xff0000, 1, 100 );
@@ -58,7 +78,7 @@ window.onload = function () {
 //                geometry.computeFaceNormals();
 //                var mesh = new THREE.Mesh( geometry, new THREE.MeshNormalMaterial({ color: 0xffff00 }) );
 
-    var line = new THREE.Line(geometry, material);
+    var line = new THREE.Line(geometryLines, materialLines);
     scene.add(line);
 
     // FIN DE RED
